bb27ebd62996a9f5247c6819c8deee840f6c47a2
ansme <hosimati_suisei@xujc.world>
2020-07-15 04:14:31 +0800

    2.1

--- main.cpp
+++ main.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 int main()
 {
-    BMP bmp("1.bmp");
-    Color c;
-    for (unsigned int i = 0; i < bmp.getHeight(); i++)
+    try
     {
-        for (unsigned int j = 0; j < bmp.getWidth(); j++)
-        {
-            c = bmp.getPixel(i, j);
-            c.R *= 2;
-            c.G *= 2;
-            c.B *= 2;
-            bmp.setPixel(i, j, c);
-        }
+        BMP bmp("2.1.bmp");
+        cout << "水平像素数：" << bmp.getWidth() << '\n';
+        cout << "垂直像素数：" << bmp.getHeight() << '\n';
+        Color c;
+        c = bmp.getPixel(0, 0);
+        cout << "左下：\nR：" << static_cast<int>(c.R) << "\nG：" << static_cast<int>(c.G) << "\nB：" << static_cast<int>(c.B) << '\n';
+        c = bmp.getPixel(bmp.getHeight() - 1, bmp.getWidth() - 1);
+        cout << "右下：\nR：" << static_cast<int>(c.R) << "\nG：" << static_cast<int>(c.G) << "\nB：" << static_cast<int>(c.B) << '\n';
+        bmp.conv2Gray();
+        bmp.save("2.1.2.bmp");
+        return 0;
     }
-    bmp.save("1.2.bmp");
-    return 0;
+    catch (const char *msg)
+    {
+        cerr << msg;
+        return -1;
+    }
 }
